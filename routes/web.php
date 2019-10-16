240da325f6fd9d7f956ba394ce768a941e1abe9e
btbell <btbell@gmail.com>
2019-10-16 11:26:39 -0400

    added a route and controller rooms

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function() {return 'Goodbye!'; });
+
+Route::get('/rooms', 'ShowRoomsController');
